5730df4ae49234736d106af15e49a69857c3f749
Toby Crisford <toby.crisford@l3harris.com>
2022-08-05 12:02:52 +0100

    Reverting new approach (loads of memory use and still quite slow) - returning to old approach but improving slightly by getting nodes to remember stuff about words that have been put down them

--- find_sets.py
+++ find_sets.py
@@ -17,6 +17,7 @@ class WordTree:
         self.layer = layer
         self.children = {}
         self.depth = depth
+        self.all_words_contain = set(ascii_lowercase)
         
     def add_word(self, word):
         
@@ -27,10 +28,14 @@ class WordTree:
             if not (relevant_letter in self.children):
                 self.children[relevant_letter] = WordTree(self.depth, layer=self.layer+1)
             self.children[relevant_letter].add_word(word)
+        self.all_words_contain = self.all_words_contain.intersection(set(word[self.layer+1:]))
                 
                 
     #Assumes unique letters in words
-    def solve(self, iteration_number, current_place, used_letters, used_words, answer_store, top_parent, order_constraint, iteration_depth, progress=False):
+    def solve(self, iteration_number, current_place, used_letters, used_words, answer_store, top_parent, order_constraint, iteration_depth):
+        
+        if iteration_number == 1 and self.layer == -1:
+            print(current_place)
         
         if self.layer == self.depth - 1:
             if iteration_number == iteration_depth - 1:
@@ -52,61 +57,51 @@ class WordTree:
                 start_place = letter_order_lookup[current_place[self.layer+1]]
             else:
                 start_place = -1
-                    
-            child_iterator = self.children
-            if progress:
-                child_iterator = tqdm(self.children)
             
-            for child in child_iterator:
+            for child in self.children:
                 if not (child in used_letters):
                     if letter_order_lookup[child] >= start_place:
-                        self.children[child].solve(iteration_number,
-                                                   current_place,
-                                                   used_letters,
-                                                   used_words,
-                                                   answer_store,
-                                                   top_parent,
-                                                   order_constraint and (letter_order_lookup[child] == start_place),
-                                                   iteration_depth)
+                        if len(used_letters.intersection(self.children[child].all_words_contain)) == 0:
+                            self.children[child].solve(iteration_number,
+    	                                                current_place,
+    	                                                used_letters,
+    	                                                used_words,
+    	                                                answer_store,
+    	                                                top_parent,
+    	                                                order_constraint and (letter_order_lookup[child] == start_place),
+    	                                                iteration_depth)
                         
                         
-word_list = open('words_alpha.txt','r').readlines()
-word_tree = WordTree(5)
 
-anagram_lookup = {} #For recovering actual words at the end if we want
+def create_initial_wordtree(filename='words_alpha.txt'):
 
-print("Creating initial word tree...")
-for w in tqdm(word_list):
-    word = w[:-1] #Remove newline character
-    if len(word) == 5:
-        if len(word) == len(set(word)):
-            sorted_word = sorted(word) #Sorting removes anagrams
-            sorted_word_string = ''.join(sorted_word)
-            if not (sorted_word_string in anagram_lookup):
-                anagram_lookup[sorted_word_string] = []
-            anagram_lookup[sorted_word_string].append(word)
+    word_list = open('words_alpha.txt','r').readlines()
+    word_tree = WordTree(5)
+
+    anagram_lookup = {} #For recovering actual words at the end if we want
+
+    for w in tqdm(word_list):
+        word = w[:-1] #Remove newline character
+        if len(word) == 5:
+            word_set = set(word)
+            if len(word) == len(word_set):
+                sorted_word = sorted(word) #Sorting removes anagrams
+                sorted_word_string = ''.join(sorted_word)
+                if not (sorted_word_string in anagram_lookup):
+                    anagram_lookup[sorted_word_string] = []
+                anagram_lookup[sorted_word_string].append(word)
             
-            word_tree.add_word(sorted_word)
+                word_tree.add_word(sorted_word)
+                
+    return word_tree, anagram_lookup
+
+
+if __name__ == "__main__":
             
-print("Word tree created")
+    print("Creating word tree...")
+    word_tree, anagram_lookup = create_initial_wordtree()
+    print("Word tree created")
 
-print("Obtaining pairs...")           
-pairs = []
-word_tree.solve(0,None,set(),[], pairs, word_tree, False, 2)
-print("Pairs obtained")
-
-print("Creating pair tree...")
-pair_tree = WordTree(10)
-for pair in tqdm(pairs):
-   pair_tree.add_word(pair[0] + pair[1])
-print("Pair tree created")
-
-print("Obtaining fours")
-fours = []
-pair_tree.solve(0,None,set(),[],fours,pair_tree,False,2,progress=True)
-print("Fours obtained")
-
-print("Obtaining fives")
-fives = []
-for four in tqdm(fours):
-    word_tree.solve(0,None,set(four[0]+four[1]),[four],fives,word_tree,False,1)
+        
+    answer_list = []
+    word_tree.solve(0,None,set(),[], answer_list, word_tree, False, 5)
