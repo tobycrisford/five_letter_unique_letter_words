193819db2e8b6520c6da17f5f5cca91a779a91fd
Toby Crisford <tobycrisford@gmail.com>
2022-08-13 12:44:37 +0100

    Creating list of only the most common words, using google book word frequencies

---
+++ all_letters_once_sentences/generate_common_word_list.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 13 11:28:59 2022
+
+@author: tobycrisford
+
+This script extracts only common words
+from the files found here: http://storage.googleapis.com/books/ngrams/books/datasetsv2.html
+and saves them.
+"""
+
+import os
+from tqdm import tqdm
+
+frequency_threshold = 500000 #Found by trial and error
+
+common_word_list = []
+for f in os.listdir('../../wordle_solver/word_frequencies'):
+    with open('../../wordle_solver/word_frequencies/' + f + '/' + f, encoding='utf-8') as file:
+        
+        frequency_dict = {}
+        for line in tqdm(file):
+            row = line.split("\t")
+            word = row[0]
+            underscore_location = word.find('_')
+            if not underscore_location == -1:
+                word = word[:underscore_location]
+            word = word.lower()
+            if word in frequency_dict:
+                frequency_dict[word] += int(row[2])
+            else:
+                frequency_dict[word] = int(row[2])
+        
+        new_common_words = [word for word in frequency_dict if frequency_dict[word] > frequency_threshold]
+        common_word_list += new_common_words
+
+
+with open('common_words.txt','w') as f:
+    for word in common_word_list:
+        f.write(word + "\n")
